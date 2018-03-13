83a7356e52cdb00fbaef2fdb8b7480cc24386165
Huyen Nguyen <nbthanhhuyen@gmail.com>
2018-03-13 18:16:36 -0400

    get started with main function...

--- dumpster-diving/rm.c
+++ dumpster-diving/rm.c
@@ -8,16 +8,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "args.h"
 
-
 void print_usage();
-
 void parse_args(int argc, char **argv);
-
+void rm_files(dev_t dumpster_device_id);
 
 int main(int argc, char **argv) {
+	static const char *ENV_NAME = "DUMPSTER"; // environment variable
+
+	// parse command line arguments
 	parse_args(argc, argv);
+
+	// check if environment variable (aka DUMPSTER) has been set
+	char *dumpster_path = getenv(ENV_NAME);
+	if (dumpster_path == NULL) {
+		fprintf(stderr, "%s\n", "Error: DUMPSTER hasn't been set. Aborting...");
+		exit(1);
+	}
+	struct stat dumpster_stat;
+	int dumpster_stat_code = stat(dumpster_path, &dumpster_stat);
+
+	// if dumpster path is invalid, report error
+	if (dumpster_stat_code == -1) {
+		perror("Dumpster Path Error ");
+		exit(1);
+	}
+
+	// main logic
+	rm_files(dumpster_stat.st_dev);
+}
+
+void rm_files(dev_t dumpster_device_id) {
+	int i;
+	for (i = 0; i < args.num_input_files; i++) {
+		char *file_path = args.input_files[i];
+		struct stat file_stat;
+		int file_stat_code = stat(file_path, &file_stat);
+
+		// if file doesn't exist, report error
+		if (file_stat_code == -1) {
+			perror("File Path Error ");
+			exit(1);
+		}
+
+		// if file is directory
+		if (S_ISDIR(file_stat.st_mode)) {
+			if (!args.rflag) {
+				fprintf(stderr, "%s\n", "Error: -r is missing for a directory. Aborting...");
+				exit(1);
+			}
+		}
+
+		// if file is on another partition
+		if (file_stat.st_dev != dumpster_device_id) {
+			
+		} else { // if file is on the same partition
+
+		}
+	}
 }
 
 void parse_args(int argc, char **argv) {
@@ -42,9 +93,18 @@ void parse_args(int argc, char **argv) {
 			break;
 		}
 	}
+	// printf("%s",*(argv+1));
 	args.num_input_files = argc - optind;
 	args.input_files = argv + optind;
+
+	// -h flag 
 	if (args.hflag) print_usage();
+
+	// check if there's input file
+	if (args.num_input_files == 0) {
+		fprintf(stderr, "%s\n", "Error: No input file specified. Aborting...");
+		exit(1);
+	}
 }
 
 /* print a usage message and quit */
