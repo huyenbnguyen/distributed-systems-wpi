55534d37e9728e8e38c7b51eef363000a366c964
Huyen Nguyen <nbthanhhuyen@gmail.com>
2018-03-23 12:59:18 -0400

    test large folder

--- dumpster-diving/test/test_script.sh
+++ dumpster-diving/test/test_script.sh
@@ -59,3 +59,148 @@ $current_path/measure_time $rm_script_path file4.txt
 $current_path/measure_time $rm_script_path file5.txt
 $current_path/measure_time $rm_script_path file6.txt
 $current_path/measure_time $rm_script_path file7.txt
+
+echo "Testing ./rm on DIFFERENT partition in milliseconds for LARGE directory"
+mkdir large
+mkdir large/sublarge1
+cd large/sublarge1
+dd if=/dev/zero of=file1.txt count=1 bs=64
+dd if=/dev/zero of=file2.txt count=1 bs=64
+dd if=/dev/zero of=file3.txt count=1 bs=64
+dd if=/dev/zero of=file4.txt count=1 bs=64
+dd if=/dev/zero of=file5.txt count=1 bs=64
+dd if=/dev/zero of=file6.txt count=1 bs=64
+dd if=/dev/zero of=file7.txt count=1 bs=64
+dd if=/dev/zero of=file8.txt count=1 bs=64
+dd if=/dev/zero of=file9.txt count=1 bs=64
+dd if=/dev/zero of=file10.txt count=1 bs=64
+dd if=/dev/zero of=file11.txt count=1 bs=64
+cd ..
+mkdir sublarge2
+cd sublarge2
+dd if=/dev/zero of=file1.txt count=1 bs=64
+dd if=/dev/zero of=file2.txt count=1 bs=64
+dd if=/dev/zero of=file3.txt count=1 bs=64
+dd if=/dev/zero of=file4.txt count=1 bs=64
+dd if=/dev/zero of=file5.txt count=1 bs=64
+dd if=/dev/zero of=file6.txt count=1 bs=64
+dd if=/dev/zero of=file7.txt count=1 bs=64
+dd if=/dev/zero of=file8.txt count=1 bs=64
+dd if=/dev/zero of=file9.txt count=1 bs=64
+dd if=/dev/zero of=file10.txt count=1 bs=64
+dd if=/dev/zero of=file11.txt count=1 bs=64
+cd ..
+mkdir sublarge3
+cd sublarge3
+dd if=/dev/zero of=file1.txt count=1 bs=64
+dd if=/dev/zero of=file2.txt count=1 bs=64
+dd if=/dev/zero of=file3.txt count=1 bs=64
+dd if=/dev/zero of=file4.txt count=1 bs=64
+dd if=/dev/zero of=file5.txt count=1 bs=64
+dd if=/dev/zero of=file6.txt count=1 bs=64
+dd if=/dev/zero of=file7.txt count=1 bs=64
+dd if=/dev/zero of=file8.txt count=1 bs=64
+dd if=/dev/zero of=file9.txt count=1 bs=64
+dd if=/dev/zero of=file10.txt count=1 bs=64
+dd if=/dev/zero of=file11.txt count=1 bs=64
+cd ..
+mkdir sublarge4
+cd sublarge4
+dd if=/dev/zero of=file1.txt count=1 bs=64
+dd if=/dev/zero of=file2.txt count=1 bs=64
+dd if=/dev/zero of=file3.txt count=1 bs=64
+dd if=/dev/zero of=file4.txt count=1 bs=64
+dd if=/dev/zero of=file5.txt count=1 bs=64
+dd if=/dev/zero of=file6.txt count=1 bs=64
+dd if=/dev/zero of=file7.txt count=1 bs=64
+dd if=/dev/zero of=file8.txt count=1 bs=64
+dd if=/dev/zero of=file9.txt count=1 bs=64
+dd if=/dev/zero of=file10.txt count=1 bs=64
+dd if=/dev/zero of=file11.txt count=1 bs=64
+cd ..
+mkdir sublarge5
+cd sublarge5
+dd if=/dev/zero of=file1.txt count=1 bs=64
+dd if=/dev/zero of=file2.txt count=1 bs=64
+dd if=/dev/zero of=file3.txt count=1 bs=64
+dd if=/dev/zero of=file4.txt count=1 bs=64
+dd if=/dev/zero of=file5.txt count=1 bs=64
+dd if=/dev/zero of=file6.txt count=1 bs=64
+dd if=/dev/zero of=file7.txt count=1 bs=64
+dd if=/dev/zero of=file8.txt count=1 bs=64
+dd if=/dev/zero of=file9.txt count=1 bs=64
+dd if=/dev/zero of=file10.txt count=1 bs=64
+dd if=/dev/zero of=file11.txt count=1 bs=64
+cd ..
+mkdir sublarge6
+cd sublarge6
+dd if=/dev/zero of=file1.txt count=1 bs=64
+dd if=/dev/zero of=file2.txt count=1 bs=64
+dd if=/dev/zero of=file3.txt count=1 bs=64
+dd if=/dev/zero of=file4.txt count=1 bs=64
+dd if=/dev/zero of=file5.txt count=1 bs=64
+dd if=/dev/zero of=file6.txt count=1 bs=64
+dd if=/dev/zero of=file7.txt count=1 bs=64
+dd if=/dev/zero of=file8.txt count=1 bs=64
+dd if=/dev/zero of=file9.txt count=1 bs=64
+dd if=/dev/zero of=file10.txt count=1 bs=64
+dd if=/dev/zero of=file11.txt count=1 bs=64
+cd ..
+mkdir sublarge7
+cd sublarge7
+dd if=/dev/zero of=file1.txt count=1 bs=64
+dd if=/dev/zero of=file2.txt count=1 bs=64
+dd if=/dev/zero of=file3.txt count=1 bs=64
+dd if=/dev/zero of=file4.txt count=1 bs=64
+dd if=/dev/zero of=file5.txt count=1 bs=64
+dd if=/dev/zero of=file6.txt count=1 bs=64
+dd if=/dev/zero of=file7.txt count=1 bs=64
+dd if=/dev/zero of=file8.txt count=1 bs=64
+dd if=/dev/zero of=file9.txt count=1 bs=64
+dd if=/dev/zero of=file10.txt count=1 bs=64
+dd if=/dev/zero of=file11.txt count=1 bs=64
+cd ..
+mkdir sublarge8
+cd sublarge8
+dd if=/dev/zero of=file1.txt count=1 bs=64
+dd if=/dev/zero of=file2.txt count=1 bs=64
+dd if=/dev/zero of=file3.txt count=1 bs=64
+dd if=/dev/zero of=file4.txt count=1 bs=64
+dd if=/dev/zero of=file5.txt count=1 bs=64
+dd if=/dev/zero of=file6.txt count=1 bs=64
+dd if=/dev/zero of=file7.txt count=1 bs=64
+dd if=/dev/zero of=file8.txt count=1 bs=64
+dd if=/dev/zero of=file9.txt count=1 bs=64
+dd if=/dev/zero of=file10.txt count=1 bs=64
+dd if=/dev/zero of=file11.txt count=1 bs=64
+cd ..
+mkdir sublarge9
+cd sublarge9
+dd if=/dev/zero of=file1.txt count=1 bs=64
+dd if=/dev/zero of=file2.txt count=1 bs=64
+dd if=/dev/zero of=file3.txt count=1 bs=64
+dd if=/dev/zero of=file4.txt count=1 bs=64
+dd if=/dev/zero of=file5.txt count=1 bs=64
+dd if=/dev/zero of=file6.txt count=1 bs=64
+dd if=/dev/zero of=file7.txt count=1 bs=64
+dd if=/dev/zero of=file8.txt count=1 bs=64
+dd if=/dev/zero of=file9.txt count=1 bs=64
+dd if=/dev/zero of=file10.txt count=1 bs=64
+dd if=/dev/zero of=file11.txt count=1 bs=64
+cd ..
+mkdir sublarge10
+cd sublarge10
+dd if=/dev/zero of=file1.txt count=1 bs=64
+dd if=/dev/zero of=file2.txt count=1 bs=64
+dd if=/dev/zero of=file3.txt count=1 bs=64
+dd if=/dev/zero of=file4.txt count=1 bs=64
+dd if=/dev/zero of=file5.txt count=1 bs=64
+dd if=/dev/zero of=file6.txt count=1 bs=64
+dd if=/dev/zero of=file7.txt count=1 bs=64
+dd if=/dev/zero of=file8.txt count=1 bs=64
+dd if=/dev/zero of=file9.txt count=1 bs=64
+dd if=/dev/zero of=file10.txt count=1 bs=64
+dd if=/dev/zero of=file11.txt count=1 bs=64
+cd ../..
+
+$current_path/measure_time $rm_script_path -r large
