85a4a3c27e26d16c90af095024719594507e46c9
Huyen Nguyen <nbthanhhuyen@gmail.com>
2018-03-31 16:41:30 -0400

    add test

---
+++ distributed-shell/test/measure_time.c
@@ -0,0 +1,39 @@
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv) {
+    // printf("%s\n", argv[1]);
+    pid_t child_pid;
+    int child_status;
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    child_pid = fork();
+    if(child_pid == 0) {
+        /* This is done by the child process. */
+        if (argc == 6) {
+            char *args[]={argv[1],argv[2],argv[3],argv[4],argv[5],NULL};
+            execvp(args[0],args);
+        }
+        /* If execvp returns, it must have failed. */
+        printf("Unknown command\n");
+        exit(0);
+    }
+    else {
+       /* This is run by the parent.  Wait for the child
+          to terminate. */
+        pid_t tpid;
+        do {
+            tpid = wait(&child_status);
+        } while(tpid != child_pid);
+        sync();
+        gettimeofday(&end, NULL);
+        printf("%ld\n", ((end.tv_sec * 1000000 + end.tv_usec)
+            - (start.tv_sec * 1000000 + start.tv_usec)));
+        printf("%s\n", "");
+        return child_status;
+     }
+}
---
+++ distributed-shell/test/test_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# compile time measuring program
+gcc measure_time.c -o measure_time
+
+current_path=`pwd`
+program_path=$current_path/../
+
+# echo "*********************"
+# echo "Starting server"
+# $program_path/server &
+echo "*********************"
+echo "Running empty command 10 times"
+for i in {1..2}
+do
+	echo ""
+	echo $i
+	./measure_time $program_path/client -s 127.0.0.1 -c ""
+done
+echo "Shutting down server..."
+./measure_time $program_path/client -s 127.0.0.1 -c "exit"
